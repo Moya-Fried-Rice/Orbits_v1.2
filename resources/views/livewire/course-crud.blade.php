1bad7d8970f4f8f13c955b2af0e675d7dc5746a8
Cj <eemo2004@gmail.com>
2025-01-04 19:01:50 +0800

    CRUD Updated

--- resources/views/livewire/course-crud.blade.php
+++ resources/views/livewire/course-crud.blade.php
@@ -23,7 +23,7 @@
             <div class="relative group w-full sm:w-[250px]">
                 <input 
                     class="px-5 py-2 pr-20 border border-[#DDD] rounded appearance-none w-full transition-all duration-200 group-hover:border-[#923534]" 
-                    wire:model="search" 
+                    wire:model.live="search" 
                     placeholder="Search..." 
                 />
                 <div class="absolute right-5 top-1/2 transform -translate-y-1/2 hover:text-blue-500 transition-colors duration-200">
