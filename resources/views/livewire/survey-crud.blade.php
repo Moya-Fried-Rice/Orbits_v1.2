7494db4e797d6871ca9baa0ab98069bbcc0b5b0d
Cj <eemo2004@gmail.com>
2025-01-14 18:23:20 +0800

    Survey

--- resources/views/livewire/survey-crud.blade.php
+++ resources/views/livewire/survey-crud.blade.php
@@ -1,20 +1,3 @@
 <div>
-    <!-- Dropdown to select the period -->
-    <select wire:model="periodId" wire:change="showSurveysForPeriod">
-        <option value="">Select Period</option>
-        @foreach($evaluationPeriods as $period)
-            <option value="{{ $period->period_id }}">{{ $period->semester }} {{ $period->academic_year }}</option>
-        @endforeach
-    </select>
-
-    <!-- Display surveys -->
-    <div>
-        <h3>Surveys for Selected Period</h3>
-       
-            <ul>
-                @foreach($surveys as $survey)
-                    <li>{{ $survey->survey_name }}</li>
-                @endforeach
-            </ul>
-    </div>
+    
 </div>
