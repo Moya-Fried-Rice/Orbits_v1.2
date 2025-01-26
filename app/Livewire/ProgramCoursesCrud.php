7c66e65539020e86c7a04a9a8d3e8d940de270b3
Moya-Fried-Rice <eemo2004@gmail.com>
2025-01-26 19:36:45 +0800

    Program Course Added

---
+++ app/Livewire/ProgramCoursesCrud.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ProgramCoursesCrud extends Component
+{
+    public function render()
+    {
+        return view('livewire.program-courses-crud');
+    }
+}
---
+++ resources/views/programs/program-courses.blade.php
@@ -0,0 +1,43 @@
+{{-- Layout Template --}}
+@extends('layouts.master')
+
+@section('title')
+
+    <title>Orbits | Program Courses</title>
+
+@endsection
+{{-- Content --}}
+@section('content')
+
+<div class="font-TT mb-2 flex justify-between items-center gap-2 hover:opacity-100 transition-all duration-100">
+
+    <div class="opacity-50 hidden md:block">
+    Program Courses
+    </div>
+
+    <div class="flex justify-between items-center gap-2">
+        <a href="{{ route('dashboard') }}" class="opacity-50 hover:opacity-100 transition-all duration-100">
+            Home
+        </a>
+        <span class="text-xs opacity-50"><i class="fa-solid fa-chevron-right"></i></span>
+        <a href="{{ route('programs') }}" class="opacity-50 hover:opacity-100 transition-all duration-100">
+            Programs
+        </a>
+        <span class="text-xs opacity-50"><i class="fa-solid fa-chevron-right"></i></span>
+        <a href="{{ route('program.courses', ['uuid' => $uuid]) }}" class="opacity-50 hover:opacity-100 transition-all duration-100">
+            Courses
+        </a>
+    </div>
+</div>
+
+    {{-- Livewire Component --}}
+    <livewire:program-courses-crud :uuid="$uuid" />
+
+@endsection
+
+{{-- Scripts --}}
+@section('scripts')
+
+    {{-- Scripts Here --}}
+    
+@endsection
