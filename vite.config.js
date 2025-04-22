456e5cbe657c5e09a49807e0462b017ff46aaea3
Moya-Fried-Rice <eemo2004@gmail.com>
2025-04-22 19:47:02 +0800

    forced https

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,22 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        https: true, // Enable HTTPS in development if needed
+        hmr: {
+            host: 'orbitsv12-production.up.railway.app',
+        },
+    },
+    build: {
+        manifest: true,
+    },
 });
