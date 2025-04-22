0bf18ea9096e1fedfbe2a3609cf8683e8fc9b6f9
Moya-Fried-Rice <eemo2004@gmail.com>
2025-04-22 19:13:58 +0800

    test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <link rel="stylesheet" href="https://orbitsv12-production.up.railway.app/assets/fonts/fontawesome-free-6.7.2-web/css/all.min.css"> --}}
     {{-- <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome-free-6.7.2-web/css/all.min.css') }}"> --}}
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/css/app.css')
+    @vite('resources/js/app.js') --}}
     @livewireStyles
 
     @yield('title')
