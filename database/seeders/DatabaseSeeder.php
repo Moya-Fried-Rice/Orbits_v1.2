f6fad99a6f95566a4d4eedf693e6b721a75ec4dc
Moya-Fried-Rice <eemo2004@gmail.com>
2025-04-24 03:27:01 +0800

    asd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\DB;
 
-DB::table('migrations')->truncate();
-
 class DatabaseSeeder extends Seeder
 {
     
@@ -16,35 +14,35 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    // public function run()
-    // {
-    //     // Add all your seeders here
-    //     $this->call([
-    //         RolesTableSeeder::class,
-    //         AdminsTableSeeder::class,
-    //         DepartmentsTableSeeder::class,
-    //         CoursesTableSeeder::class,
-    //         ProgramsTableSeeder::class,
-    //         SectionsTableSeeder::class,
-    //         CourseSectionsTableSeeder::class,
-    //         FacultiesTableSeeder::class,
-    //         ProgramChairsTableSeeder::class,
-    //         ProgramCoursesTableSeeder::class,
-    //         SurveysTableSeeder::class,
-    //         SurveyRoleTableSeeder::class,
-    //         QuestionCriteriaTableSeeder::class,
-    //         QuestionsTableSeeder::class,
-    //         StudentsTableSeeder::class,
-    //     ]);
-    // }
-
     public function run()
     {
-        // Get the contents of your SQL file
-        $path = database_path('orbits_v1_2.sql');
-        $sql = File::get($path);
-        
-        // Execute the SQL commands
-        DB::unprepared($sql);
+        // Add all your seeders here
+        $this->call([
+            RolesTableSeeder::class,
+            AdminsTableSeeder::class,
+            DepartmentsTableSeeder::class,
+            CoursesTableSeeder::class,
+            ProgramsTableSeeder::class,
+            SectionsTableSeeder::class,
+            CourseSectionsTableSeeder::class,
+            FacultiesTableSeeder::class,
+            ProgramChairsTableSeeder::class,
+            ProgramCoursesTableSeeder::class,
+            SurveysTableSeeder::class,
+            SurveyRoleTableSeeder::class,
+            QuestionCriteriaTableSeeder::class,
+            QuestionsTableSeeder::class,
+            StudentsTableSeeder::class,
+        ]);
     }
+
+    // public function run()
+    // {
+    //     // Get the contents of your SQL file
+    //     $path = database_path('orbits_v1_2.sql');
+    //     $sql = File::get($path);
+        
+    //     // Execute the SQL commands
+    //     DB::unprepared($sql);
+    // }
 }
